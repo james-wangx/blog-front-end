316714236640f399ccebe334c80651ed79a4bac7
James Wang <james_wangx@163.com>
2022-11-06 21:10:45 +0800

    不用 JS 函数填充登录框

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,27 @@
 // noinspection HtmlUnknownTarget
 
-/**
- * 填充登录框
- */
-function fillElement() {
-    let login = localStorage.getItem("login");
-    let divLogin = document.querySelector(".login");
-    if (login === "true") {
-        divLogin.innerHTML = `<select>
+let itemLogin = localStorage.getItem("login");
+let divLogin = document.querySelector(".login");
+if (itemLogin === "true") {
+    divLogin.innerHTML = `<select>
                               <option value="1">Admin</option>
                               <option value="2">退出登录</option>
                           </select>
     `;
-        // 点击选项退出登录
-        let select = divLogin.querySelector("select");
-        select.onchange = function () {
-            if (select.value === '2') {
-                if (window.confirm("确认退出登录？")) {
-                    localStorage.removeItem("login");
-                    location.reload();
-                } else {
-                    select.value = '1';
-                }
+    // 点击选项退出登录
+    let select = divLogin.querySelector("select");
+    select.onchange = function () {
+        if (select.value === '2') {
+            if (window.confirm("确认退出登录？")) {
+                localStorage.removeItem("login");
+                location.reload();
+            } else {
+                select.value = '1';
             }
         }
-    } else {
-        divLogin.innerHTML = "<a href='login.html'>登录/注册</a>"
     }
+} else {
+    divLogin.innerHTML = "<a href='login.html'>登录/注册</a>"
 }
 
 
